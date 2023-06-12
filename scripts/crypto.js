02252f6c1667b3acf58d655cdb0a0ec2b2c7f176
Sebastian Pikand <sebastianpikand@gmail.com>
2023-06-12 17:00:33 +0300

    code review

--- scripts/crypto.js
+++ scripts/crypto.js
@@ -3,12 +3,8 @@ async function getCryptoPrices() {
   if (cryptoSymbol) {
     addLoader();
     try {
-      const response = await fetch(
-        "https://data.binance.com/api/v3/ticker/24hr?symbol=" +
-          cryptoSymbol.toUpperCase() +
-          "EUR"
-      );
-      const data = await response.json();
+      const apiEndpoint = `...`;
+      const data = await fetch(apiEndpoint).then((res) => res.json());
       removeLoader();
       return data;
     } catch (error) {
@@ -18,25 +14,25 @@ async function getCryptoPrices() {
 }
 
 function showUserInputField() {
-  const usetTextInput = document.getElementById("user-text-input");
-  if (document.getElementById("symbol").value === "Other") {
-    usetTextInput.style.display = "block";
+  const usetTextInput = document.getElementById('user-text-input');
+  if (document.getElementById('symbol').value === 'Other') {
+    usetTextInput.style.display = 'block';
   } else {
-    usetTextInput.style.display = "none";
+    usetTextInput.style.display = 'none';
   }
 }
 
 function handleUserInput() {
-  if (document.getElementById("symbol").value === "Other") {
-    userInput = document.getElementById("user-text-input").value;
+  if (document.getElementById('symbol').value === 'Other') {
+    userInput = document.getElementById('user-text-input').value;
     if (validateUserInput(userInput)) {
-      document.getElementById("error-container").style.display = "none";
+      document.getElementById('error-container').style.display = 'none';
       return userInput;
     } else {
-      showErrorMessage("Not a valid crypto symbol");
+      showErrorMessage('Not a valid crypto symbol');
     }
   } else {
-    return document.getElementById("symbol").value;
+    return document.getElementById('symbol').value;
   }
 }
 
@@ -46,57 +42,42 @@ function validateUserInput(userInput) {
 }
 
 function showErrorMessage(message) {
-  const errorContainer = document.getElementById("error-container");
-  errorContainer.style.display = "block";
+  const errorContainer = document.getElementById('error-container');
+  errorContainer.style.display = 'block';
   errorContainer.innerHTML = message;
-  hideCryptoDataContainer();
+  document.getElementById('crypto-data-container').style.display = 'none';
   removeLoader();
 }
 
 async function getCryptoDataJson() {
   // Build a json file with user's specified parameters about crypto data.
-  let = jsonData = await getCryptoPrices();
+  let jsonData = await getCryptoPrices();
 
-  let output = "";
-  const checkboxContainers =
-    document.getElementsByClassName("checkbox-container");
+  let output = '';
+  const checkboxContainers = document.getElementsByClassName('checkbox-container');
   for (let i = 0; i < checkboxContainers.length; i++) {
-    let isCheckboxChecked =
-      checkboxContainers[i].getElementsByClassName("check-input")[0].checked;
+    let isCheckboxChecked = checkboxContainers[i].getElementsByClassName('check-input')[0].checked;
 
     if (isCheckboxChecked) {
-      let checkboxValue =
-        checkboxContainers[i].getElementsByTagName("span")[0].innerHTML;
-      let checkBoxId =
-        checkboxContainers[i].getElementsByClassName("check-input")[0].id;
-      output += `<li>${checkboxValue}: ${Number(jsonData[checkBoxId]).toFixed(
-        4
-      )}</li>`;
+      let checkboxValue = checkboxContainers[i].getElementsByTagName('span')[0].innerHTML;
+      let checkBoxId = checkboxContainers[i].getElementsByClassName('check-input')[0].id;
+      output += `<li>${checkboxValue}: ${Number(jsonData[checkBoxId]).toFixed(4)}</li>`;
     }
     showCryptoDataContainer(output);
   }
 }
 
 function addLoader() {
-  document.getElementById("crypto-data-list").innerHTML = "";
-  document.getElementById("crypto-data-container").innerHTML +=
-    '<div class="loader"></div>';
+  document.getElementById('crypto-data-list').innerHTML = '';
+  document.getElementById('crypto-data-container').innerHTML += '<div class="loader"></div>';
 }
 
 function removeLoader() {
-  document
-    .getElementById("crypto-data-container")
-    .removeChild(document.querySelector(".loader"));
+  document.getElementById('crypto-data-container').removeChild(document.querySelector('.loader'));
 }
 
 function showCryptoDataContainer(cryptoData) {
-  document.getElementById("crypto-data-container").style.display = "block";
-  document.getElementById(
-    "crypto-data-container-title"
-  ).innerHTML = `Data for ${jsonData["symbol"]}`;
-  document.getElementById("crypto-data-list").innerHTML = cryptoData;
-}
-
-function hideCryptoDataContainer() {
-  document.getElementById("crypto-data-container").style.display = "none";
+  document.getElementById('crypto-data-container').style.display = 'block';
+  document.getElementById('crypto-data-container-title').innerHTML = `Data for ${jsonData['symbol']}`;
+  document.getElementById('crypto-data-list').innerHTML = cryptoData;
 }
